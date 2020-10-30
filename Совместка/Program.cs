6a1f0c42214a0b127887976879b6caafb1eb8af7
Egor <egor.rumyancev.9494@mail.ru>
2020-10-30 20:05:36 +0500

    Chess

---
+++ Совместка/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Совместка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
